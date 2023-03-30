7778c2b77caa7dbbe207ab56ba5a2bd2fdce12f4
Yu Takaoka <pinokions009@gmail.com>
2023-03-30 18:45:11 +0900

    default value of index_type

--- TypedIndex.py
+++ TypedIndex.py
@@ -39,24 +39,6 @@ class TypedMultiIndexType(Generic[TD]):
 class TypedMultiIndex(pd.MultiIndex, Generic[TD]):
     pass
 
-IDX = TypeVar('IDX', bound = GenericIndexType | TypedMultiIndexType)
+GIT_INT: GenericIndexType[int] = GenericIndexType(int)
 
-class IndexType(Generic[IDX]):
-    def __init__(self, idx_type: IDX) -> None:
-        self.idx_type = idx_type
-
-    def typecheck(self, idx: pd.Index) -> "TypeGuard[IndexType[IDX]]":
-        if isinstance(self.idx_type, GenericIndexType):             
-            return conv_typename(str(idx.dtype)) == self.idx_type.t.__name__
-
-        def _get_datatypes(idx: pd.Index) -> dict[str, Type]:
-            return {str(k): conv_typename(str(t)) for k, t in idx.to_frame().dtypes.to_dict().items()}
-        
-        dt: dict[str, str] = {k: v.__name__ for k, v in self.idx_type.td.__annotations__.items()}
-        return _get_datatypes(idx) == dt
-
-    def __call__(self, idx: pd.Index) -> "IndexType[IDX]":
-        if not self.typecheck(idx):
-            raise TypeError('Typecheck failed.')
-        
-        return idx
+IDX = TypeVar('IDX', GenericIndexType, GenericIndexType[int], TypedMultiIndexType)
--- example/TypedDF_Example2.py
+++ example/TypedDF_Example2.py
@@ -2,20 +2,13 @@ from TypedDF import *
 from example.Human import Human
 from example.Human2 import Human2
 
-class IdxType1(TypedDict):
-    name: str
-    age: int
-
-TMI1: TypedMultiIndexType[IdxType1] = TypedMultiIndexType(IdxType1)
-GIT: GenericIndexType[int] = GenericIndexType(int)
-
 def main():
     df: pd.DataFrame = pd.read_csv('example/human_list.csv')
-    tdf: TypedDF[GenericIndexType[int], Human] = TypedDF.from_df(df, Human, GIT)  # OK
+    tdf: TypedDF[GenericIndexType[int], Human] = TypedDF.from_df(df, Human)  # OK
     print(tdf)
 
     df2: pd.DataFrame = pd.read_csv('example/human_list2.csv')
-    tdf2: TypedDF[GenericIndexType[int], Human2] = TypedDF.from_df(df2, Human2, GIT) # OK
+    tdf2: TypedDF[GenericIndexType[int], Human2] = TypedDF.from_df(df2, Human2) # OK
     print(tdf2)
 
     tdf_a: TypedDF[GenericIndexType[int], Human2] = tdf   # Static type check fails!
@@ -24,13 +17,13 @@ def main():
     print(tdf2_a)
 
     try:
-        tdf3: TypedDF[GenericIndexType[int], Human2] = TypedDF.from_df(df, Human2, GIT) #Runtime error!
+        tdf3: TypedDF[GenericIndexType[int], Human2] = TypedDF.from_df(df, Human2) #Runtime error!
         print(tdf3)
     except TypeError as e:
         print(e)
     
     try:
-        tdf4: TypedDF[GenericIndexType[int], Human] = TypedDF.from_df(df2, Human, GIT) #Runtime error!
+        tdf4: TypedDF[GenericIndexType[int], Human] = TypedDF.from_df(df2, Human) #Runtime error!
         print(tdf4)
     except TypeError as e:
         print(e)
