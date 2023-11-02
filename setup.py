37cb1ddd82ba7472b65adca54194e6a76dcc83ee
Yu Takaoka <pinokions009@gmail.com>
2023-11-02 14:01:07 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="typeddf",
+    version="1.0.0",
+    install_requires=[
+        "pandas",
+        "typing",
+        "typing_extensions",
+        "datetime",
+    ],
+)
